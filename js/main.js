c94b6ec481903cdb55ac82facaa71fc9be2a3efc
Vyacheslav <ryzkovslava538@gmail.com>
2025-12-07 15:08:19 +0500

    Выполнено задание Помощь друга

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 import './form.js';
+import { initScale } from './scale.js';
+import { initEffects } from './effects.js';
 import { generatePhotos } from './generate.js';
 import { renderPictures } from './pictures.js';
+
+initScale();
+initEffects();
 
 const photoDescriptions = generatePhotos();
 renderPictures(photoDescriptions);
